58dba5522c3314854fff9ff6be432fce13f17c05
stashymane <aestheticrice@gmail.com>
2021-05-05 11:59:48 +0300

    Update list scroll defaults

--- src/main/java/dev/stashy/extrasounds/SoundConfig.java
+++ src/main/java/dev/stashy/extrasounds/SoundConfig.java
@@ -23,7 +23,7 @@ public class SoundConfig implements ConfigData
     @ConfigEntry.Gui.CollapsibleObject
     public SoundSource inventoryClose = new SoundSource(SoundEvents.UI_TOAST_OUT, 0.2f, 1.5f, 0.1f);
     @ConfigEntry.Gui.CollapsibleObject
-    public SoundSource listScroll = new SoundSource(SoundEvents.BLOCK_NOTE_BLOCK_HAT, 0.1f, 1.5f, 0.3f);
+    public SoundSource listScroll = new SoundSource(SoundEvents.BLOCK_NOTE_BLOCK_HAT, 0.1f, 1f, 0.3f);
 
     @ConfigEntry.Gui.CollapsibleObject
     public SoundSource itemDrop = new SoundSource(SoundEvents.BLOCK_DISPENSER_LAUNCH, 0.1f, 1.5f, 0.1f);
