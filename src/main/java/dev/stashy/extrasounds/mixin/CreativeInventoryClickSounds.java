7ba4229de01b6ac01a4900f7de159492c39cedbb
stashymane <aestheticrice@gmail.com>
2021-05-15 20:36:19 +0300

    Update CreativeInventoryClickSounds.java

--- src/main/java/dev/stashy/extrasounds/mixin/CreativeInventoryClickSounds.java
+++ src/main/java/dev/stashy/extrasounds/mixin/CreativeInventoryClickSounds.java
@@ -1,11 +1,13 @@
 package dev.stashy.extrasounds.mixin;
 
 import dev.stashy.extrasounds.ExtraSounds;
-import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.ingame.AbstractInventoryScreen;
 import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen;
+import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.screen.slot.Slot;
 import net.minecraft.screen.slot.SlotActionType;
+import net.minecraft.text.Text;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -15,26 +17,32 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(CreativeInventoryScreen.class)
 public abstract class CreativeInventoryClickSounds
+        extends AbstractInventoryScreen<CreativeInventoryScreen.CreativeScreenHandler>
 {
-    @Shadow
-    public abstract int getSelectedTab();
+    public CreativeInventoryClickSounds(CreativeInventoryScreen.CreativeScreenHandler screenHandler, PlayerInventory playerInventory, Text text)
+    {
+        super(screenHandler, playerInventory, text);
+    }
 
     @Shadow
-    protected abstract boolean isCreativeInventorySlot(@Nullable Slot slot);
+    @Nullable
+    private Slot deleteItemSlot;
+
+    @Shadow
+    private static int selectedTab;
 
     @Inject(at = @At("RETURN"), method = "onMouseClick")
     void slotClick(Slot slot, int invSlot, int clickData, SlotActionType actionType, CallbackInfo ci)
     {
-        if (MinecraftClient.getInstance().player == null || slot == null || !isCreativeInventorySlot(slot))
-            return;
-        ExtraSounds.inventoryClick(slot.getStack(), MinecraftClient.getInstance().player.inventory.getCursorStack(),
-                                   actionType);
+        if (slot == null || client == null || client.player == null) return;
+            ExtraSounds.inventoryClick(slot.getStack(), client.player.inventory.getCursorStack(),
+                                       actionType);
     }
 
     @Inject(at = @At("INVOKE"), method = "setSelectedTab")
     void tabChange(ItemGroup group, CallbackInfo ci)
     {
-        if (getSelectedTab() != -1 && group.getIndex() != getSelectedTab())
+        if (selectedTab != -1 && group.getIndex() != selectedTab)
             ExtraSounds.playItemSound(group.getIcon(), true);
     }
 }
