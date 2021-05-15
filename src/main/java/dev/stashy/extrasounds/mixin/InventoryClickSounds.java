f963ba61b8048846d24efe10624d87ba10f2d390
stashymane <aestheticrice@gmail.com>
2021-05-15 20:28:59 +0300

    Fix crashing on inventory clicks

--- src/main/java/dev/stashy/extrasounds/mixin/InventoryClickSounds.java
+++ src/main/java/dev/stashy/extrasounds/mixin/InventoryClickSounds.java
@@ -1,27 +1,30 @@
 package dev.stashy.extrasounds.mixin;
 
 import dev.stashy.extrasounds.ExtraSounds;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.screen.ScreenHandler;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.screen.slot.Slot;
 import net.minecraft.screen.slot.SlotActionType;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ScreenHandler.class)
-public abstract class InventoryClickSounds
+@Mixin(HandledScreen.class)
+public class InventoryClickSounds
 {
-    @Shadow
-    public abstract Slot getSlot(int index);
 
-    @Inject(at = @At("RETURN"), method = "onSlotClick")
-    void click(int i, int j, SlotActionType actionType, PlayerEntity playerEntity, CallbackInfoReturnable<ItemStack> cir)
+    @Shadow
+    @Final
+    protected PlayerInventory playerInventory;
+
+    @Inject(at = @At("INVOKE"), method = "onMouseClick")
+    void click(Slot slot, int invSlot, int clickData, SlotActionType actionType, CallbackInfo ci)
     {
-        if (i >= 0)
-            ExtraSounds.inventoryClick(getSlot(i).getStack(), playerEntity.inventory.getCursorStack(), actionType);
+        System.out.println("Clicked: " + invSlot);
+        if (slot != null)
+            ExtraSounds.inventoryClick(slot.getStack(), playerInventory.getCursorStack(), actionType);
     }
 }
