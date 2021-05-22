53a79767dd8d82efbd8507dabfde22c756f03a09
stashymane <aestheticrice@gmail.com>
2021-05-23 01:33:26 +0300

    Add item pick sound

---
+++ src/main/java/dev/stashy/extrasounds/mixin/ItemPickSound.java
@@ -0,0 +1,31 @@
+package dev.stashy.extrasounds.mixin;
+
+import dev.stashy.extrasounds.ExtraSounds;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerInventory.class)
+public class ItemPickSound
+{
+    @Shadow
+    @Final
+    public PlayerEntity player;
+
+    @Inject(method = "addPickBlock", at = {
+            @At(value = "INVOKE", target = "Lnet/minecraft/util/collection/DefaultedList;set(ILjava/lang/Object;)Ljava/lang/Object;"),
+            @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerInventory;swapSlotWithHotbar(I)V"),
+            @At(value = "FIELD", target = "Lnet/minecraft/entity/player/PlayerInventory;selectedSlot:I")
+    })
+    void pickSound(ItemStack stack, CallbackInfo ci)
+    {
+        if (!player.getMainHandStack().getItem().equals(stack.getItem()))
+            ExtraSounds.playItemSound(stack, true);
+    }
+}
