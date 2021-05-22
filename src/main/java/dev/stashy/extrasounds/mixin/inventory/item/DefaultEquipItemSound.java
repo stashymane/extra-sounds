797b06e4d33ca9faec10e028beff800aacf4acf4
stashymane <aestheticrice@gmail.com>
2021-05-23 01:32:55 +0300

    ADD MORE SOUNDS

--- src/main/java/dev/stashy/extrasounds/mixin/inventory/item/DefaultEquipItemSound.java
+++ src/main/java/dev/stashy/extrasounds/mixin/inventory/item/DefaultEquipItemSound.java
@@ -2,14 +2,14 @@ package dev.stashy.extrasounds.mixin.inventory.item;
 
 import dev.stashy.extrasounds.InventorySound;
 import dev.stashy.extrasounds.ItemSoundContainer;
-import net.minecraft.item.FlintAndSteelItem;
+import net.minecraft.item.CompassItem;
 import net.minecraft.sound.SoundEvents;
 import org.spongepowered.asm.mixin.Mixin;
 
-@Mixin(FlintAndSteelItem.class)
-public class FlintAndSteelItemSound implements ItemSoundContainer
+@Mixin({CompassItem.class})
+public class DefaultEquipItemSound implements ItemSoundContainer
 {
-    InventorySound snd = new InventorySound(SoundEvents.ITEM_ARMOR_EQUIP_NETHERITE, 1f);
+    InventorySound snd = new InventorySound(SoundEvents.ITEM_ARMOR_EQUIP_GENERIC, 1f);
 
     @Override
     public InventorySound getInventorySound()
--- src/main/java/dev/stashy/extrasounds/mixin/inventory/item/DefaultIronItemSound.java
+++ src/main/java/dev/stashy/extrasounds/mixin/inventory/item/DefaultIronItemSound.java
@@ -2,15 +2,16 @@ package dev.stashy.extrasounds.mixin.inventory.item;
 
 import dev.stashy.extrasounds.InventorySound;
 import dev.stashy.extrasounds.ItemSoundContainer;
+import net.minecraft.item.FlintAndSteelItem;
+import net.minecraft.item.ShearsItem;
 import net.minecraft.item.ShieldItem;
 import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 
-@Mixin(ShieldItem.class)
-public class ShieldItemSound implements ItemSoundContainer
+@Mixin({FlintAndSteelItem.class, ShearsItem.class, ShieldItem.class})
+public class DefaultIronItemSound implements ItemSoundContainer
 {
-    InventorySound snd = new InventorySound(SoundEvents.ITEM_ARMOR_EQUIP_IRON, 1);;
+    InventorySound snd = new InventorySound(SoundEvents.ITEM_ARMOR_EQUIP_IRON, 1f);
 
     @Override
     public InventorySound getInventorySound()
