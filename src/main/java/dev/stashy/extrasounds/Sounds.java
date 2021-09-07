fe531a0c6d8f00857802367b4f8968277cc1f40d
stashymane <aestheticrice@gmail.com>
2021-09-07 03:00:35 +0300

    Change sound IDs

--- src/main/java/dev/stashy/extrasounds/Sounds.java
+++ src/main/java/dev/stashy/extrasounds/Sounds.java
@@ -6,40 +6,40 @@ import net.minecraft.util.registry.Registry;
 
 public class Sounds
 {
-    public static final Identifier CHAT_ID = new Identifier("extrasounds:chat");
+    public static final Identifier CHAT_ID = new Identifier("extrasounds:chat.message");
     public static SoundEvent CHAT = new SoundEvent(CHAT_ID);
 
-    public static final Identifier CHAT_MENTION_ID = new Identifier("extrasounds:chat_mention");
+    public static final Identifier CHAT_MENTION_ID = new Identifier("extrasounds:chat.mention");
     public static SoundEvent CHAT_MENTION = new SoundEvent(CHAT_MENTION_ID);
 
     public static final Identifier HOTBAR_SCROLL_ID = new Identifier("extrasounds:hotbar_scroll");
     public static SoundEvent HOTBAR_SCROLL = new SoundEvent(HOTBAR_SCROLL_ID);
 
-    public static final Identifier INVENTORY_OPEN_ID = new Identifier("extrasounds:inventory_open");
+    public static final Identifier INVENTORY_OPEN_ID = new Identifier("extrasounds:inventory.open");
     public static SoundEvent INVENTORY_OPEN = new SoundEvent(INVENTORY_OPEN_ID);
 
-    public static final Identifier INVENTORY_CLOSE_ID = new Identifier("extrasounds:inventory_close");
+    public static final Identifier INVENTORY_CLOSE_ID = new Identifier("extrasounds:inventory.close");
     public static SoundEvent INVENTORY_CLOSE = new SoundEvent(INVENTORY_CLOSE_ID);
 
-    public static final Identifier INVENTORY_SCROLL_ID = new Identifier("extrasounds:inventory_scroll");
+    public static final Identifier INVENTORY_SCROLL_ID = new Identifier("extrasounds:inventory.scroll");
     public static SoundEvent INVENTORY_SCROLL = new SoundEvent(INVENTORY_SCROLL_ID);
 
-    public static final Identifier ITEM_DROP_ID = new Identifier("extrasounds:item_drop");
+    public static final Identifier ITEM_DROP_ID = new Identifier("extrasounds:item.drop");
     public static SoundEvent ITEM_DROP = new SoundEvent(ITEM_DROP_ID);
 
-    public static final Identifier ITEM_PICK_ID = new Identifier("extrasounds:item_pickup");
+    public static final Identifier ITEM_PICK_ID = new Identifier("extrasounds:item.pickup");
     public static SoundEvent ITEM_PICK = new SoundEvent(ITEM_PICK_ID);
 
-    public static final Identifier ITEM_PICK_ALL_ID = new Identifier("extrasounds:item_pickup_all");
+    public static final Identifier ITEM_PICK_ALL_ID = new Identifier("extrasounds:item.pickup_all");
     public static SoundEvent ITEM_PICK_ALL = new SoundEvent(ITEM_PICK_ALL_ID);
 
-    public static final Identifier ITEM_CLONE_ID = new Identifier("extrasounds:item_clone");
+    public static final Identifier ITEM_CLONE_ID = new Identifier("extrasounds:item.clone");
     public static SoundEvent ITEM_CLONE = new SoundEvent(ITEM_CLONE_ID);
 
-    public static final Identifier ITEM_DELETE_ID = new Identifier("extrasounds:item_delete");
+    public static final Identifier ITEM_DELETE_ID = new Identifier("extrasounds:item.delete");
     public static SoundEvent ITEM_DELETE = new SoundEvent(ITEM_DELETE_ID);
 
-    public static final Identifier ITEM_DRAG_ID = new Identifier("extrasounds:item_drag");
+    public static final Identifier ITEM_DRAG_ID = new Identifier("extrasounds:item.drag");
     public static SoundEvent ITEM_DRAG = new SoundEvent(ITEM_DRAG_ID);
 
     protected static void registerAll()
