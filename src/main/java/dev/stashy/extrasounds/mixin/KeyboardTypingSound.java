5dd0db88339d52a09758d78b32fa58c5dfee9f3f
stashymane <aestheticrice@gmail.com>
2021-10-25 16:45:30 +0300

    Implement typing sound

---
+++ src/main/java/dev/stashy/extrasounds/mixin/KeyboardTypingSound.java
@@ -0,0 +1,20 @@
+package dev.stashy.extrasounds.mixin;
+
+import dev.stashy.extrasounds.ExtraSounds;
+import dev.stashy.extrasounds.Sounds;
+import net.minecraft.client.gui.widget.TextFieldWidget;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(TextFieldWidget.class)
+public class KeyboardTypingSound
+{
+    @Inject(at = @At("RETURN"), method = "charTyped")
+    public void type(char chr, int modifiers, CallbackInfoReturnable<Boolean> cir)
+    {
+        if (cir.getReturnValue())
+            ExtraSounds.playSound(Sounds.KEYBOARD_TYPE);
+    }
+}
