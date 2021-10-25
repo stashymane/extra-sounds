9e7d9357afadb432a1268cb17d03e553379915b7
stashymane <aestheticrice@gmail.com>
2021-10-25 03:40:02 +0300

    Fix repeated effect sounds

--- src/main/java/dev/stashy/extrasounds/mixin/EffectMixin.java
+++ src/main/java/dev/stashy/extrasounds/mixin/EffectMixin.java
@@ -1,29 +1,33 @@
 package dev.stashy.extrasounds.mixin;
 
 import dev.stashy.extrasounds.ExtraSounds;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.attribute.AttributeContainer;
 import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(StatusEffect.class)
+@Mixin(LivingEntity.class)
 public abstract class EffectMixin
 {
 
-    @Inject(at = @At("TAIL"), method = "onApplied")
-    public void addEffect(LivingEntity entity, AttributeContainer attributes, int amplifier, CallbackInfo ci)
+    @Inject(at = @At("HEAD"), method = "onStatusEffectApplied")
+    public void addEffect(StatusEffectInstance effect, Entity source, CallbackInfo ci)
     {
-        if (entity.isPlayer() && entity.hasStatusEffect((StatusEffect) (Object) this))
-            ExtraSounds.playEffectSound((StatusEffect) (Object) this, true);
+        if ((LivingEntity) (Object) this instanceof ClientPlayerEntity)
+            ExtraSounds.playEffectSound(effect.getEffectType(), true);
     }
 
-    @Inject(at = @At("TAIL"), method = "onRemoved")
-    public void removeEffect(LivingEntity entity, AttributeContainer attributes, int amplifier, CallbackInfo ci)
+    @Inject(at = @At("HEAD"), method = "removeStatusEffectInternal")
+    public void removeEffect(StatusEffect type, CallbackInfoReturnable<StatusEffectInstance> cir)
     {
-        if (entity.isPlayer() && !entity.hasStatusEffect((StatusEffect) (Object) this))
-            ExtraSounds.playEffectSound((StatusEffect) (Object) this, false);
+        if ((LivingEntity) (Object) this instanceof ClientPlayerEntity)
+            //if (entity.isPlayer() && !entity.hasStatusEffect((StatusEffect) (Object) this))
+            ExtraSounds.playEffectSound(type, false);
     }
 }
