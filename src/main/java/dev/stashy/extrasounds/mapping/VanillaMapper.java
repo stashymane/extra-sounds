5fb90af3836d7f5e1bddc058121c1aee3aeb2ca6
stashymane <aestheticrice@gmail.com>
2021-10-28 18:44:44 +0300

    Add vanilla item/block mapper

---
+++ src/main/java/dev/stashy/extrasounds/mapping/VanillaMapper.java
@@ -0,0 +1,116 @@
+package dev.stashy.extrasounds.mapping;
+
+import dev.stashy.extrasounds.ExtraSounds;
+import dev.stashy.extrasounds.mixin.BucketFluidAccessor;
+import net.minecraft.block.AbstractRailBlock;
+import net.minecraft.block.BannerBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.LeavesBlock;
+import net.minecraft.client.sound.SoundEntry;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.item.*;
+import net.minecraft.util.registry.Registry;
+
+import static dev.stashy.extrasounds.sounds.Categories.*;
+import static dev.stashy.extrasounds.sounds.Sounds.*;
+
+public class VanillaMapper
+{
+    static
+    {
+        //Vanilla mapper
+        SoundMapper.register(ExtraSounds.MODID, id -> {
+            Item i = Registry.ITEM.get(id);
+            if (i instanceof BlockItem)
+                return block((BlockItem) i);
+            else
+                return item(i);
+        });
+    }
+
+    private static SoundEntry item(Item i)
+    {
+        if (i instanceof MusicDiscItem)
+            return aliased(MUSIC_DISC);
+        else if (i instanceof BoatItem)
+            return aliased(BOAT);
+        else if (i instanceof ToolItem t)
+        {
+            if (t.getMaterial() instanceof ToolMaterials mat)
+                switch (mat)
+                {
+                    case WOOD -> {return aliased(Gear.WOOD);}
+                    case STONE -> {return aliased(Gear.STONE);}
+                    case IRON -> {return aliased(Gear.IRON);}
+                    case GOLD -> {return aliased(Gear.GOLDEN);}
+                    case DIAMOND -> {return aliased(Gear.DIAMOND);}
+                    case NETHERITE -> {return aliased(Gear.NETHERITE);}
+                    default -> {return aliased(Gear.GENERIC);}
+                }
+            else
+                return aliased(Gear.GENERIC);
+        }
+        else if (i instanceof ArmorItem a)
+        {
+            if (a.getMaterial() instanceof ArmorMaterials mat)
+                switch (mat)
+                {
+                    case IRON -> {return aliased(Gear.IRON);}
+                    case GOLD -> {return aliased(Gear.GOLDEN);}
+                    case DIAMOND -> {return aliased(Gear.DIAMOND);}
+                    case NETHERITE -> {return aliased(Gear.NETHERITE);}
+                    case CHAIN -> {return aliased(Gear.CHAIN);}
+                    case TURTLE -> {return aliased(Gear.TURTLE);}
+                    case LEATHER -> {return aliased(Gear.LEATHER);}
+                    default -> {return aliased(Gear.GENERIC);}
+                }
+            else
+                return aliased(Gear.GENERIC);
+        }
+        else if (i instanceof ShieldItem)
+            return aliased(Gear.IRON);
+        else if (i instanceof HorseArmorItem a || i instanceof CompassItem || i instanceof SpyglassItem || i instanceof ShearsItem)
+            return aliased(Gear.GOLDEN);
+        else if (i instanceof LeadItem || i instanceof ElytraItem || i instanceof SaddleItem)
+            return aliased(Gear.LEATHER);
+        else if (i instanceof BowItem || i instanceof CrossbowItem || i instanceof FishingRodItem || i instanceof OnAStickItem)
+            return aliased(Gear.GENERIC);
+        else if (i instanceof BucketItem b)
+        {
+            Fluid f = ((BucketFluidAccessor) b).getFluid();
+            return f.getBucketFillSound().isPresent() ?
+                    event(f.getBucketFillSound().get().getId()) : aliased(METAL);
+        }
+        else if (i instanceof MinecartItem)
+            return aliased(MINECART);
+        else if (i instanceof ItemFrameItem)
+            return aliased(FRAME);
+        else if (i instanceof PotionItem || i instanceof ExperienceBottleItem)
+            return aliased(POTION);
+        else if (i instanceof BannerPatternItem || i instanceof BookItem || i instanceof WritableBookItem || i instanceof WrittenBookItem
+                || i instanceof EnchantedBookItem || i instanceof EmptyMapItem || i instanceof FilledMapItem || i instanceof NameTagItem)
+            return aliased(PAPER);
+        else if (i instanceof ArrowItem)
+            return aliased(ARROW);
+        else if (i instanceof DyeItem)
+            return aliased(DUST);
+        else if (i instanceof SpawnEggItem)
+            return aliased(WET_SLIPPERY);
+        else if (i instanceof MushroomStewItem || i instanceof SuspiciousStewItem)
+            return aliased(BOWL);
+
+        return aliased(ITEM_PICK);
+    }
+
+    private static SoundEntry block(BlockItem i)
+    {
+        Block b = i.getBlock();
+        if (b instanceof AbstractRailBlock)
+            return aliased(RAIL);
+        else if (b instanceof BannerBlock)
+            return aliased(BANNER);
+        else if (b instanceof LeavesBlock)
+            return aliased(LEAVES);
+        return event(b.getSoundGroup(b.getDefaultState()).getPlaceSound().getId());
+    }
+}
