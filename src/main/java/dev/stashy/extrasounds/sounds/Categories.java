4634d13b6fbb3ac604fa4ac88c698f912ddf2a73
stashymane <aestheticrice@gmail.com>
2021-10-28 18:17:28 +0300

    Add categories as SoundEvents

---
+++ src/main/java/dev/stashy/extrasounds/sounds/Categories.java
@@ -0,0 +1,43 @@
+package dev.stashy.extrasounds.sounds;
+
+import net.minecraft.sound.SoundEvent;
+
+public class Categories extends SoundRegistry
+{
+    public static SoundEvent ARROW = register("item.category.arrow");
+    public static SoundEvent BANNER = register("item.category.banner");
+    public static SoundEvent BOAT = register("item.category.boat");
+    public static SoundEvent BOWL = register("item.category.bowl");
+    public static SoundEvent CRUNCHY_DRY = register("item.category.crunchy_dry");
+    public static SoundEvent DUST = register("item.category.dust");
+    public static SoundEvent FOOD_DRY = register("item.category.food_dry");
+    public static SoundEvent FRAME = register("item.category.frame");
+    public static SoundEvent HAY = register("item.category.hay");
+    public static SoundEvent LEAVES = register("item.category.leaves");
+    public static SoundEvent LOOSE_METAL = register("item.category.loose_metal");
+    public static SoundEvent MEAT = register("item.category.meat");
+    public static SoundEvent METAL = register("item.category.metal");
+    public static SoundEvent METAL_BITS = register("item.category.metal_bits");
+    public static SoundEvent METAL_SHEETS = register("item.category.metal_sheets");
+    public static SoundEvent MINECART = register("item.category.minecart");
+    public static SoundEvent MUSIC_DISC = register("item.category.music_disc");
+    public static SoundEvent PAPER = register("item.category.paper");
+    public static SoundEvent POTION = register("item.category.potion");
+    public static SoundEvent RAIL = register("item.category.rail");
+    public static SoundEvent WET = register("item.category.wet");
+    public static SoundEvent WET_SLIPPERY = register("item.category.wet_slippery");
+
+    public static class Gear
+    {
+        public static SoundEvent CHAIN = register("item.category.gear.chain");
+        public static SoundEvent DIAMOND = register("item.category.gear.diamond");
+        public static SoundEvent GENERIC = register("item.category.gear.generic");
+        public static SoundEvent GOLDEN = register("item.category.gear.golden");
+        public static SoundEvent IRON = register("item.category.gear.iron");
+        public static SoundEvent LEATHER = register("item.category.gear.leather");
+        public static SoundEvent NETHERITE = register("item.category.gear.netherite");
+        public static SoundEvent STONE = register("item.category.gear.stone");
+        public static SoundEvent TURTLE = register("item.category.gear.turtle");
+        public static SoundEvent WOOD = register("item.category.gear.wood");
+    }
+}
