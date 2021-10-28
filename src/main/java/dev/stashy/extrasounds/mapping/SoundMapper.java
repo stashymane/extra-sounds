ad4d680added16e86334035214abc083ba7bb710
stashymane <aestheticrice@gmail.com>
2021-10-28 18:44:33 +0300

    Add SoundMapper

---
+++ src/main/java/dev/stashy/extrasounds/mapping/SoundMapper.java
@@ -0,0 +1,23 @@
+package dev.stashy.extrasounds.mapping;
+
+import dev.stashy.extrasounds.ExtraSounds;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.client.sound.SoundEntry;
+import net.minecraft.util.Identifier;
+
+import java.util.function.Function;
+
+public record SoundMapper(String modId,
+                          Function<Identifier, SoundEntry> itemSoundMapper)
+{
+    public static SoundMapper of(String modId, Function<Identifier, SoundEntry> itemSoundMapper)
+    {
+        return new SoundMapper(modId, itemSoundMapper);
+    }
+
+    public static void register(String modId, Function<Identifier, SoundEntry> itemSoundMapper)
+    {
+        FabricLoader.getInstance().getEntrypoints(ExtraSounds.MODID, SoundMapper.class)
+                    .add(SoundMapper.of(modId, itemSoundMapper));
+    }
+}
