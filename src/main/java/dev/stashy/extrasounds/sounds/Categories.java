123ab02eb1d245e50f01c07e95296b65c9f9a74c
stashymane <aestheticrice@gmail.com>
2021-10-30 10:53:54 +0300

    Make all sounds final

--- src/main/java/dev/stashy/extrasounds/sounds/Categories.java
+++ src/main/java/dev/stashy/extrasounds/sounds/Categories.java
@@ -4,40 +4,40 @@ import net.minecraft.sound.SoundEvent;
 
 public class Categories extends SoundRegistry
 {
-    public static SoundEvent ARROW = register("item.category.arrow");
-    public static SoundEvent BANNER = register("item.category.banner");
-    public static SoundEvent BOAT = register("item.category.boat");
-    public static SoundEvent BOWL = register("item.category.bowl");
-    public static SoundEvent CRUNCHY_DRY = register("item.category.crunchy_dry");
-    public static SoundEvent DUST = register("item.category.dust");
-    public static SoundEvent FOOD_DRY = register("item.category.food_dry");
-    public static SoundEvent FRAME = register("item.category.frame");
-    public static SoundEvent HAY = register("item.category.hay");
-    public static SoundEvent LEAVES = register("item.category.leaves");
-    public static SoundEvent LOOSE_METAL = register("item.category.loose_metal");
-    public static SoundEvent MEAT = register("item.category.meat");
-    public static SoundEvent METAL = register("item.category.metal");
-    public static SoundEvent METAL_BITS = register("item.category.metal_bits");
-    public static SoundEvent METAL_SHEETS = register("item.category.metal_sheets");
-    public static SoundEvent MINECART = register("item.category.minecart");
-    public static SoundEvent MUSIC_DISC = register("item.category.music_disc");
-    public static SoundEvent PAPER = register("item.category.paper");
-    public static SoundEvent POTION = register("item.category.potion");
-    public static SoundEvent RAIL = register("item.category.rail");
-    public static SoundEvent WET = register("item.category.wet");
-    public static SoundEvent WET_SLIPPERY = register("item.category.wet_slippery");
+    public static final SoundEvent ARROW = register("item.category.arrow");
+    public static final SoundEvent BANNER = register("item.category.banner");
+    public static final SoundEvent BOAT = register("item.category.boat");
+    public static final SoundEvent BOWL = register("item.category.bowl");
+    public static final SoundEvent CRUNCHY_DRY = register("item.category.crunchy_dry");
+    public static final SoundEvent DUST = register("item.category.dust");
+    public static final SoundEvent FOOD_DRY = register("item.category.food_dry");
+    public static final SoundEvent FRAME = register("item.category.frame");
+    public static final SoundEvent HAY = register("item.category.hay");
+    public static final SoundEvent LEAVES = register("item.category.leaves");
+    public static final SoundEvent LOOSE_METAL = register("item.category.loose_metal");
+    public static final SoundEvent MEAT = register("item.category.meat");
+    public static final SoundEvent METAL = register("item.category.metal");
+    public static final SoundEvent METAL_BITS = register("item.category.metal_bits");
+    public static final SoundEvent METAL_SHEETS = register("item.category.metal_sheets");
+    public static final SoundEvent MINECART = register("item.category.minecart");
+    public static final SoundEvent MUSIC_DISC = register("item.category.music_disc");
+    public static final SoundEvent PAPER = register("item.category.paper");
+    public static final SoundEvent POTION = register("item.category.potion");
+    public static final SoundEvent RAIL = register("item.category.rail");
+    public static final SoundEvent WET = register("item.category.wet");
+    public static final SoundEvent WET_SLIPPERY = register("item.category.wet_slippery");
 
     public static class Gear
     {
-        public static SoundEvent CHAIN = register("item.category.gear.chain");
-        public static SoundEvent DIAMOND = register("item.category.gear.diamond");
-        public static SoundEvent GENERIC = register("item.category.gear.generic");
-        public static SoundEvent GOLDEN = register("item.category.gear.golden");
-        public static SoundEvent IRON = register("item.category.gear.iron");
-        public static SoundEvent LEATHER = register("item.category.gear.leather");
-        public static SoundEvent NETHERITE = register("item.category.gear.netherite");
-        public static SoundEvent STONE = register("item.category.gear.stone");
-        public static SoundEvent TURTLE = register("item.category.gear.turtle");
-        public static SoundEvent WOOD = register("item.category.gear.wood");
+        public static final SoundEvent CHAIN = register("item.category.gear.chain");
+        public static final SoundEvent DIAMOND = register("item.category.gear.diamond");
+        public static final SoundEvent GENERIC = register("item.category.gear.generic");
+        public static final SoundEvent GOLDEN = register("item.category.gear.golden");
+        public static final SoundEvent IRON = register("item.category.gear.iron");
+        public static final SoundEvent LEATHER = register("item.category.gear.leather");
+        public static final SoundEvent NETHERITE = register("item.category.gear.netherite");
+        public static final SoundEvent STONE = register("item.category.gear.stone");
+        public static final SoundEvent TURTLE = register("item.category.gear.turtle");
+        public static final SoundEvent WOOD = register("item.category.gear.wood");
     }
 }
--- src/main/java/dev/stashy/extrasounds/sounds/Sounds.java
+++ src/main/java/dev/stashy/extrasounds/sounds/Sounds.java
@@ -9,23 +9,23 @@ import java.util.List;
 
 public class Sounds extends SoundRegistry
 {
-    public static SoundEvent CHAT = register("chat.message");
-    public static SoundEvent CHAT_MENTION = register("chat.mention");
-    public static SoundEvent HOTBAR_SCROLL = register("hotbar_scroll");
-    public static SoundEvent INVENTORY_OPEN = register("inventory.open");
-    public static SoundEvent INVENTORY_CLOSE = register("inventory.close");
-    public static SoundEvent INVENTORY_SCROLL = register("inventory.scroll");
-    public static SoundEvent ITEM_DROP = register("item.drop");
-    public static SoundEvent ITEM_PICK = register("item.pickup");
-    public static SoundEvent ITEM_PICK_ALL = register("item.pickup_all");
-    public static SoundEvent ITEM_CLONE = register("item.clone");
-    public static SoundEvent ITEM_DELETE = register("item.delete");
-    public static SoundEvent ITEM_DRAG = register("item.drag");
-    public static SoundEvent EFFECT_ADD_POSITIVE = register("effect.add.positive");
-    public static SoundEvent EFFECT_ADD_NEGATIVE = register("effect.add.negative");
-    public static SoundEvent EFFECT_REMOVE_POSITIVE = register("effect.remove.positive");
-    public static SoundEvent EFFECT_REMOVE_NEGATIVE = register("effect.remove.negative");
-    public static SoundEvent KEYBOARD_TYPE = register("keyboard.type");
+    public static final SoundEvent CHAT = register("chat.message");
+    public static final SoundEvent CHAT_MENTION = register("chat.mention");
+    public static final SoundEvent HOTBAR_SCROLL = register("hotbar_scroll");
+    public static final SoundEvent INVENTORY_OPEN = register("inventory.open");
+    public static final SoundEvent INVENTORY_CLOSE = register("inventory.close");
+    public static final SoundEvent INVENTORY_SCROLL = register("inventory.scroll");
+    public static final SoundEvent ITEM_DROP = register("item.drop");
+    public static final SoundEvent ITEM_PICK = register("item.pickup");
+    public static final SoundEvent ITEM_PICK_ALL = register("item.pickup_all");
+    public static final SoundEvent ITEM_CLONE = register("item.clone");
+    public static final SoundEvent ITEM_DELETE = register("item.delete");
+    public static final SoundEvent ITEM_DRAG = register("item.drag");
+    public static final SoundEvent EFFECT_ADD_POSITIVE = register("effect.add.positive");
+    public static final SoundEvent EFFECT_ADD_NEGATIVE = register("effect.add.negative");
+    public static final SoundEvent EFFECT_REMOVE_POSITIVE = register("effect.remove.positive");
+    public static final SoundEvent EFFECT_REMOVE_NEGATIVE = register("effect.remove.negative");
+    public static final SoundEvent KEYBOARD_TYPE = register("keyboard.type");
 
     public static SoundEntry aliased(SoundEvent e)
     {
