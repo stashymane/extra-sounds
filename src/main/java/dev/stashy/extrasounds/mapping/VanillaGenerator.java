25ed900c5091330d889f4256614f06a4007fa46e
stashymane <aestheticrice@gmail.com>
2021-10-31 18:32:57 +0200

    Refactor vanilla generator to use maps
    Less verbose than if/else spaghetti

--- src/main/java/dev/stashy/extrasounds/mapping/VanillaGenerator.java
+++ src/main/java/dev/stashy/extrasounds/mapping/VanillaGenerator.java
@@ -6,32 +6,34 @@ import net.minecraft.block.BannerBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.LeavesBlock;
 import net.minecraft.client.sound.SoundEntry;
-import net.minecraft.fluid.Fluid;
 import net.minecraft.item.*;
 import net.minecraft.util.registry.Registry;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
 import static dev.stashy.extrasounds.sounds.Categories.*;
 import static dev.stashy.extrasounds.sounds.Sounds.*;
 
 public class VanillaGenerator
 {
+    public static final Map<Class<? extends Item>, Function<Item, SoundEntry>> map = new HashMap<>();
+
     public static SoundGenerator generator = SoundGenerator.of("minecraft", id -> {
         Item i = Registry.ITEM.get(id);
-        if (i instanceof BlockItem)
-            return block((BlockItem) i);
-        else
-            return item(i);
+        Class<? extends Item> cls = i.getClass();
+        while (!map.containsKey(cls) && cls.getSuperclass() != null && cls.getSuperclass().isAssignableFrom(Item.class))
+            cls = (Class<? extends Item>) cls.getSuperclass();
+        return map.containsKey(cls) ? map.get(cls).apply(i) : aliased(ITEM_PICK);
     });
 
-    private static SoundEntry item(Item i)
+    static
     {
-        if (i instanceof MusicDiscItem)
-            return aliased(MUSIC_DISC);
-        else if (i instanceof BoatItem)
-            return aliased(BOAT);
-        else if (i instanceof ToolItem t)
-        {
-            if (t.getMaterial() instanceof ToolMaterials mat)
+        map.put(MusicDiscItem.class, it -> aliased(MUSIC_DISC));
+        map.put(BoatItem.class, it -> aliased(BOAT));
+        map.put(ToolItem.class, it -> {
+            if (((ToolItem) it).getMaterial() instanceof ToolMaterials mat)
                 return switch (mat)
                         {
                             case WOOD -> aliased(Gear.WOOD);
@@ -41,13 +43,13 @@ public class VanillaGenerator
                             case DIAMOND -> aliased(Gear.DIAMOND);
                             case NETHERITE -> aliased(Gear.NETHERITE);
                             default -> aliased(Gear.GENERIC);
+                            //⬆ even though not required, this is in case any mods add to the enum of materials
                         };
             else
                 return aliased(Gear.GENERIC);
-        }
-        else if (i instanceof ArmorItem a)
-        {
-            if (a.getMaterial() instanceof ArmorMaterials mat)
+        });
+        map.put(ArmorItem.class, it -> {
+            if (((ArmorItem) it).getMaterial() instanceof ArmorMaterials mat)
                 return switch (mat)
                         {
                             case IRON -> aliased(Gear.IRON);
@@ -61,51 +63,44 @@ public class VanillaGenerator
                         };
             else
                 return aliased(Gear.GENERIC);
-        }
-        else if (i instanceof ShieldItem)
-            return aliased(Gear.IRON);
-        else if (i instanceof HorseArmorItem a || i instanceof CompassItem || i instanceof SpyglassItem || i instanceof ShearsItem)
-            return aliased(Gear.GOLDEN);
-        else if (i instanceof LeadItem || i instanceof ElytraItem || i instanceof SaddleItem)
-            return aliased(Gear.LEATHER);
-        else if (i instanceof BowItem || i instanceof CrossbowItem || i instanceof FishingRodItem || i instanceof OnAStickItem)
-            return aliased(Gear.GENERIC);
-        else if (i instanceof BucketItem b)
-        {
-            Fluid f = ((BucketFluidAccessor) b).getFluid();
+        });
+        map.put(ShieldItem.class, it -> aliased(Gear.IRON));
+        putMulti(it -> aliased(Gear.GOLDEN), HorseArmorItem.class, CompassItem.class, SpyglassItem.class,
+                 ShearsItem.class);
+        putMulti(it -> aliased(Gear.LEATHER), LeadItem.class, ElytraItem.class, SaddleItem.class);
+        putMulti(it -> aliased(Gear.GENERIC), BowItem.class, CrossbowItem.class, FishingRodItem.class,
+                 OnAStickItem.class);
+        map.put(BucketItem.class, it -> {
+            var f = ((BucketFluidAccessor) it).getFluid();
             return f.getBucketFillSound().isPresent() ?
                     event(f.getBucketFillSound().get().getId()) : aliased(METAL);
-        }
-        else if (i instanceof MinecartItem)
-            return aliased(MINECART);
-        else if (i instanceof ItemFrameItem)
-            return aliased(FRAME);
-        else if (i instanceof PotionItem || i instanceof ExperienceBottleItem)
-            return aliased(POTION);
-        else if (i instanceof BannerPatternItem || i instanceof BookItem || i instanceof WritableBookItem || i instanceof WrittenBookItem
-                || i instanceof EnchantedBookItem || i instanceof EmptyMapItem || i instanceof FilledMapItem || i instanceof NameTagItem)
-            return aliased(PAPER);
-        else if (i instanceof ArrowItem)
-            return aliased(ARROW);
-        else if (i instanceof DyeItem)
-            return aliased(DUST);
-        else if (i instanceof SpawnEggItem)
-            return aliased(WET_SLIPPERY);
-        else if (i instanceof MushroomStewItem || i instanceof SuspiciousStewItem)
-            return aliased(BOWL);
-
-        return aliased(ITEM_PICK);
+        });
+        map.put(MinecartItem.class, it -> aliased(MINECART));
+        map.put(ItemFrameItem.class, it -> aliased(FRAME));
+        putMulti(it -> aliased(POTION), PotionItem.class, ExperienceBottleItem.class);
+        putMulti(it -> aliased(PAPER), BannerPatternItem.class, BookItem.class, WritableBookItem.class,
+                 WrittenBookItem.class,
+                 EnchantedBookItem.class, EmptyMapItem.class, FilledMapItem.class, NameTagItem.class);
+        map.put(ArrowItem.class, it -> aliased(ARROW));
+        map.put(DyeItem.class, it -> aliased(DUST));
+        map.put(SpawnEggItem.class, it -> aliased(WET_SLIPPERY));
+        putMulti(it -> aliased(BOWL), MushroomStewItem.class, SuspiciousStewItem.class);
+        map.put(BlockItem.class, it -> {
+            Block b = ((BlockItem) it).getBlock();
+            if (b instanceof AbstractRailBlock)
+                return aliased(RAIL);
+            else if (b instanceof BannerBlock)
+                return aliased(BANNER);
+            else if (b instanceof LeavesBlock)
+                return aliased(LEAVES);
+            return event(b.getSoundGroup(b.getDefaultState()).getPlaceSound().getId());
+        });
     }
 
-    private static SoundEntry block(BlockItem i)
+    @SafeVarargs
+    private static void putMulti(Function<Item, SoundEntry> entry, Class<? extends Item>... classez)
     {
-        Block b = i.getBlock();
-        if (b instanceof AbstractRailBlock)
-            return aliased(RAIL);
-        else if (b instanceof BannerBlock)
-            return aliased(BANNER);
-        else if (b instanceof LeavesBlock)
-            return aliased(LEAVES);
-        return event(b.getSoundGroup(b.getDefaultState()).getPlaceSound().getId());
+        for (var clazz : classez)
+            map.put(clazz, entry);
     }
 }
