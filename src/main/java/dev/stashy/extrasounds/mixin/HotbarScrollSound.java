65aadc31b1ec3f1a12c8a94bb82b776f56778743
stashymane <aestheticrice@gmail.com>
2022-02-27 18:37:06 +0200

    Implement hotbar scroll sound based on item
    Implements #37

--- src/main/java/dev/stashy/extrasounds/mixin/HotbarScrollSound.java
+++ src/main/java/dev/stashy/extrasounds/mixin/HotbarScrollSound.java
@@ -1,10 +1,9 @@
 package dev.stashy.extrasounds.mixin;
 
 import dev.stashy.extrasounds.ExtraSounds;
-import dev.stashy.extrasounds.Mixers;
-import dev.stashy.extrasounds.sounds.Sounds;
 import net.minecraft.entity.player.PlayerInventory;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -12,9 +11,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(PlayerInventory.class)
 public class HotbarScrollSound
 {
+    @Shadow
+    public int selectedSlot;
+
     @Inject(at = @At("RETURN"), method = "scrollInHotbar")
     private void hotbarSound(CallbackInfo info)
     {
-        ExtraSounds.playSound(Sounds.HOTBAR_SCROLL, Mixers.SCROLL);
+        ExtraSounds.hotbar(selectedSlot);
     }
 }
