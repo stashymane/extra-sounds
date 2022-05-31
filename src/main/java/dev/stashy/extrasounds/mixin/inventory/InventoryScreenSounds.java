411f1564089d61527dd386e5213eeadfb3b52bad
stashymane <aestheticrice@gmail.com>
2022-05-31 12:10:06 +0300

    Fix #50

--- src/main/java/dev/stashy/extrasounds/mixin/inventory/InventoryScreenSounds.java
+++ src/main/java/dev/stashy/extrasounds/mixin/inventory/InventoryScreenSounds.java
@@ -3,24 +3,30 @@ package dev.stashy.extrasounds.mixin.inventory;
 import dev.stashy.extrasounds.Mixers;
 import dev.stashy.extrasounds.SoundManager;
 import dev.stashy.extrasounds.sounds.Sounds;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(HandledScreen.class)
-public class InventoryScreenSounds
+@Mixin(MinecraftClient.class)
+public abstract class InventoryScreenSounds
 {
-    @Inject(at = @At("HEAD"), method = "init")
-    void open(CallbackInfo ci)
-    {
-        SoundManager.playSound(Sounds.INVENTORY_OPEN, 1f, Mixers.INVENTORY);
-    }
 
-    @Inject(at = @At("HEAD"), method = "close")
-    void close(CallbackInfo ci)
+    @Shadow
+    @Nullable
+    public Screen currentScreen;
+
+    @Inject(at = @At("HEAD"), method = "setScreen")
+    void open(@Nullable Screen screen, CallbackInfo ci)
     {
-        SoundManager.playSound(Sounds.INVENTORY_CLOSE, 1f, Mixers.INVENTORY);
+        if (currentScreen != screen && screen instanceof HandledScreen)
+            SoundManager.playSound(Sounds.INVENTORY_OPEN, 1f, Mixers.INVENTORY);
+        else if (screen == null && currentScreen instanceof HandledScreen)
+            SoundManager.playSound(Sounds.INVENTORY_CLOSE, 1f, Mixers.INVENTORY);
     }
 }
