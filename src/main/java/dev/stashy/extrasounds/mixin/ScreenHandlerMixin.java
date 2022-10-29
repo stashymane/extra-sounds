7e3173d137407ba4c089cca6571d70d4c3204616
stashymane <aestheticrice@gmail.com>
2022-10-29 23:04:33 +0300

    Implement inventory screen sounds

---
+++ src/main/java/dev/stashy/extrasounds/mixin/ScreenHandlerMixin.java
@@ -0,0 +1,108 @@
+package dev.stashy.extrasounds.mixin;
+
+import dev.stashy.extrasounds.handlers.InventoryEventHandler;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.screen.ScreenHandler;
+import net.minecraft.screen.slot.Slot;
+import net.minecraft.screen.slot.SlotActionType;
+import net.minecraft.util.ClickType;
+import net.minecraft.util.collection.DefaultedList;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+@Mixin(ScreenHandler.class)
+public abstract class ScreenHandlerMixin
+{
+    @Shadow
+    @Final
+    public DefaultedList<Slot> slots;
+
+    @Shadow
+    public abstract ItemStack getCursorStack();
+
+    @Inject(at = @At(value = "INVOKE", shift = At.Shift.AFTER, target = "Lnet/minecraft/screen/ScreenHandler;setCursorStack(Lnet/minecraft/item/ItemStack;)V"), method = "method_34249")
+    void pickup(Slot slot, PlayerEntity playerEntity, ItemStack stack, CallbackInfo ci)
+    {
+        if (!stack.isEmpty())
+            InventoryEventHandler.INSTANCE.click(slot.getStack().getItem(), getCursorStack().getItem(),
+                                                 SlotActionType.PICKUP);
+    }
+
+    @Inject(at = @At(value = "INVOKE", shift = At.Shift.AFTER, target = "Lnet/minecraft/screen/ScreenHandler;setCursorStack(Lnet/minecraft/item/ItemStack;)V"), method = "internalOnSlotClick")
+    void click(int slotIndex, int button, SlotActionType actionType, PlayerEntity player, CallbackInfo ci)
+    {
+        Item clicked = Items.AIR;
+        System.out.println(slotIndex);
+        if (slotIndex >= 0)
+            clicked = slots.get(slotIndex).getStack().getItem();
+
+        InventoryEventHandler.INSTANCE.click(clicked, getCursorStack().getItem(),
+                                             actionType);
+    }
+
+    @Inject(at = @At(value = "INVOKE", shift = At.Shift.AFTER, target = "Lnet/minecraft/item/ItemStack;increment(I)V"), method = "internalOnSlotClick")
+    void transferAll(int slotIndex, int button, SlotActionType actionType, PlayerEntity player, CallbackInfo ci)
+    {
+        if (slotIndex >= 0)
+            InventoryEventHandler.INSTANCE.click(slots.get(slotIndex).getStack().getItem(), getCursorStack().getItem(),
+                                                 actionType);
+    }
+
+    @Inject(at = @At(value = "INVOKE_ASSIGN", ordinal = 0, target = "Lnet/minecraft/screen/ScreenHandler;transferSlot(Lnet/minecraft/entity/player/PlayerEntity;I)Lnet/minecraft/item/ItemStack;"), method = "internalOnSlotClick", locals = LocalCapture.CAPTURE_FAILSOFT)
+    void transfer(int slotIndex, int button, SlotActionType actionType, PlayerEntity player, CallbackInfo ci, ClickType clickType, Slot slot, ItemStack itemStack)
+    {
+        InventoryEventHandler.INSTANCE.click(itemStack.getItem(), getCursorStack().getItem(), actionType);
+    }
+}
+
+@Mixin(HandledScreen.class)
+abstract
+class InventoryKeyPressSound<T extends ScreenHandler>
+{
+    @Shadow
+    @Nullable
+    protected Slot focusedSlot;
+
+    @Shadow
+    public abstract T getScreenHandler();
+
+    @Shadow
+    @Final
+    protected T handler;
+
+    @Inject(at = @At(value = "INVOKE", ordinal = 0, target = "Lnet/minecraft/client/gui/screen/ingame/HandledScreen;onMouseClick(Lnet/minecraft/screen/slot/Slot;IILnet/minecraft/screen/slot/SlotActionType;)V"), method = "handleHotbarKeyPressed")
+    void inventoryHandSwap(int keyCode, int scanCode, CallbackInfoReturnable<Boolean> cir)
+    {
+        if (focusedSlot != null && focusedSlot.hasStack())
+            InventoryEventHandler.INSTANCE.click(Items.AIR, focusedSlot.getStack().getItem(), SlotActionType.SWAP);
+    }
+
+    @Inject(at = @At(value = "INVOKE", ordinal = 1, target = "Lnet/minecraft/client/gui/screen/ingame/HandledScreen;onMouseClick(Lnet/minecraft/screen/slot/Slot;IILnet/minecraft/screen/slot/SlotActionType;)V"), method = "handleHotbarKeyPressed", locals = LocalCapture.CAPTURE_FAILSOFT)
+    void inventorySlotSwap(int keyCode, int scanCode, CallbackInfoReturnable<Boolean> cir, int i)
+    {
+        if (focusedSlot != null && focusedSlot.hasStack())
+        {
+            System.out.println("focusedSlot");
+            InventoryEventHandler.INSTANCE.click(Items.AIR, focusedSlot.getStack().getItem(), SlotActionType.SWAP);
+        }
+        else if (MinecraftClient.getInstance().player != null)
+        {
+            System.out.println("handStack");
+            var stack = MinecraftClient.getInstance().player.getInventory().main.get(i);
+            if (!stack.isEmpty())
+                InventoryEventHandler.INSTANCE.click(stack.getItem(), Items.AIR, SlotActionType.SWAP);
+        }
+    }
+}
---
+++ src/main/kotlin/dev/stashy/extrasounds/handlers/InventoryEventHandler.kt
@@ -0,0 +1,30 @@
+package dev.stashy.extrasounds.handlers
+
+import dev.stashy.extrasounds.SoundManager
+import dev.stashy.extrasounds.sounds.SoundType
+import dev.stashy.extrasounds.sounds.Sounds
+import net.minecraft.item.Item
+import net.minecraft.item.ItemStack
+import net.minecraft.item.Items
+import net.minecraft.screen.slot.SlotActionType
+
+object InventoryEventHandler {
+    fun click(clicked: Item, hand: Item, eventType: SlotActionType) {
+        println("^ clicked: ${clicked.name.string} | hand: ${hand.name.string} | eventType: $eventType")
+
+        when (eventType) {
+            SlotActionType.PICKUP, SlotActionType.QUICK_MOVE, SlotActionType.SWAP -> {
+                if (clicked != Items.AIR) SoundManager.playItemSound(clicked, SoundType.PLACE)
+                else if (hand != Items.AIR) SoundManager.playItemSound(hand, SoundType.PICKUP)
+            }
+
+            SlotActionType.QUICK_CRAFT -> SoundManager.playItemSound(hand, SoundType.PLACE)
+
+            SlotActionType.PICKUP_ALL -> SoundManager.playSound(Sounds.ITEM_PICK_ALL, SoundType.PICKUP)
+            SlotActionType.CLONE -> SoundManager.playSound(Sounds.ITEM_CLONE, SoundType.PICKUP)
+
+            else -> {}
+        }
+    }
+
+}
