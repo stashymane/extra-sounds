e8b4c9662b8bbd6a73e730707136453e30d66056
stashymane <aestheticrice@gmail.com>
2022-10-30 19:43:17 +0200

    Rework vanilla generator to when block

--- src/main/kotlin/dev/stashy/extrasounds/mapping/VanillaGenerator.kt
+++ src/main/kotlin/dev/stashy/extrasounds/mapping/VanillaGenerator.kt
@@ -27,29 +27,28 @@ import net.minecraft.block.*
 import net.minecraft.client.sound.Sound
 import net.minecraft.client.sound.SoundEntry
 import net.minecraft.item.*
-import net.minecraft.util.registry.Registry
 import kotlin.jvm.optionals.getOrNull
 
 
 @OptIn(ExperimentalStdlibApi::class)
 object VanillaGenerator {
-    private val map: MutableMap<Class<out Item>, (Item) -> SoundEntry> = mutableMapOf()
-
     @JvmField
     val generator: SoundGenerator = SoundGenerator("minecraft", MODID) {
-        val id = Registry.ITEM.getId(it)
-        var cls: Class<out Item> = it.javaClass
-        while (!map.containsKey(cls) && cls.superclass != null && Item::class.java.isAssignableFrom(cls.superclass))
-            cls = cls.superclass as Class<out Item>
-        return@SoundGenerator ItemSound(map[cls]?.invoke(it) ?: aliased(ITEM_PICK))
+        ItemSound(parse(it))
     }
 
-    init {
-        map[MusicDiscItem::class.java] = { aliased(MUSIC_DISC) }
-        map[BoatItem::class.java] = { aliased(BOAT) }
-        map[ToolItem::class.java] = {
-            if ((it as ToolItem).material is ToolMaterials)
-                when (it.material) {
+    fun parse(item: Item): SoundEntry = when (item) {
+        is MusicDiscItem -> {
+            aliased(MUSIC_DISC)
+        }
+
+        is BoatItem -> {
+            aliased(BOAT)
+        }
+
+        is ToolItem -> {
+            if (item.material is ToolMaterials)
+                when (item.material) {
                     ToolMaterials.WOOD -> aliased(Categories.Gear.WOOD)
                     ToolMaterials.STONE -> aliased(Categories.Gear.STONE)
                     ToolMaterials.IRON -> aliased(Categories.Gear.IRON)
@@ -60,9 +59,10 @@ object VanillaGenerator {
                 }
             else aliased(Categories.Gear.GENERIC)
         }
-        map[ArmorItem::class.java] = {
-            if ((it as ArmorItem).material is ArmorMaterials) {
-                when (it.material) {
+
+        is ArmorItem -> {
+            if (item.material is ArmorMaterials) {
+                when (item.material) {
                     ArmorMaterials.IRON -> aliased(Categories.Gear.IRON)
                     ArmorMaterials.GOLD -> aliased(Categories.Gear.GOLDEN)
                     ArmorMaterials.DIAMOND -> aliased(Categories.Gear.DIAMOND)
@@ -74,51 +74,67 @@ object VanillaGenerator {
                 }
             } else aliased(Categories.Gear.GENERIC)
         }
-        map[ShieldItem::class.java] = { aliased(Categories.Gear.IRON) }
-        putMulti(
-            { aliased(Categories.Gear.GOLDEN) },
-            HorseArmorItem::class.java,
-            CompassItem::class.java,
-            SpyglassItem::class.java,
-            ShearsItem::class.java
-        )
-        putMulti(
-            { aliased(Categories.Gear.LEATHER) },
-            LeadItem::class.java,
-            ElytraItem::class.java,
-            SaddleItem::class.java
-        )
-        putMulti(
-            { aliased(Categories.Gear.GENERIC) },
-            BowItem::class.java,
-            CrossbowItem::class.java,
-            FishingRodItem::class.java,
-            OnAStickItem::class.java
-        )
-        map[BucketItem::class.java] = {
-            (it as BucketAccessor).fluid.bucketFillSound.getOrNull()?.let { event(it.id, 0.4f) } ?: aliased(METAL)
+
+        is ShieldItem -> {
+            aliased(Categories.Gear.IRON)
         }
-        map[MinecartItem::class.java] = { aliased(MINECART) }
-        map[ItemFrameItem::class.java] = { aliased(FRAME) }
-        putMulti({ aliased(POTION) }, PotionItem::class.java, ExperienceBottleItem::class.java)
-        putMulti(
-            { aliased(PAPER) },
-            BannerPatternItem::class.java,
-            BookItem::class.java,
-            WritableBookItem::class.java,
-            WrittenBookItem::class.java,
-            EnchantedBookItem::class.java,
-            EmptyMapItem::class.java,
-            FilledMapItem::class.java,
-            NameTagItem::class.java
-        )
-        map[ArrowItem::class.java] = { aliased(ARROW) }
-        map[DyeItem::class.java] = { aliased(DUST) }
-        map[SpawnEggItem::class.java] = { aliased(WET_SLIPPERY) }
-        putMulti({ aliased(BOWL) }, StewItem::class.java, SuspiciousStewItem::class.java)
-        map[GoatHornItem::class.java] = { single(LOOSE_METAL.id, 0.6f, 0.9f, Sound.RegistrationType.SOUND_EVENT) }
-        map[BlockItem::class.java] = {
-            val b = (it as BlockItem).block
+
+        is HorseArmorItem, is CompassItem, is SpyglassItem, is ShearsItem -> {
+            aliased(Categories.Gear.GOLDEN)
+        }
+
+        is LeadItem, is ElytraItem, is SaddleItem -> {
+            aliased(Categories.Gear.LEATHER)
+        }
+
+        is BowItem, is CrossbowItem, is FishingRodItem, is OnAStickItem<*> -> {
+            aliased(Categories.Gear.GENERIC)
+        }
+
+        is BucketItem -> {
+            (item as BucketAccessor).fluid.bucketFillSound.getOrNull()?.let { event(it.id, 0.4f) } ?: aliased(METAL)
+        }
+
+        is MinecartItem -> {
+            aliased(MINECART)
+        }
+
+        is ItemFrameItem -> {
+            aliased(FRAME)
+        }
+
+        is PotionItem, is ExperienceBottleItem -> {
+            aliased(POTION)
+        }
+
+        is BannerPatternItem, is BookItem, is WritableBookItem, is WrittenBookItem,
+        is EnchantedBookItem, is EmptyMapItem, is FilledMapItem, is NameTagItem,
+        -> {
+            aliased(PAPER)
+        }
+
+        is ArrowItem -> {
+            aliased(ARROW)
+        }
+
+        is DyeItem -> {
+            aliased(DUST)
+        }
+
+        is SpawnEggItem -> {
+            aliased(WET_SLIPPERY)
+        }
+
+        is StewItem, is SuspiciousStewItem -> {
+            aliased(BOWL)
+        }
+
+        is GoatHornItem -> {
+            single(LOOSE_METAL.id, 0.6f, 0.9f, Sound.RegistrationType.SOUND_EVENT)
+        }
+
+        is BlockItem -> {
+            val b = item.block
             val id = b.getSoundGroup(b.defaultState).placeSound.id
             when (b) {
                 is AbstractRailBlock -> aliased(RAIL)
@@ -134,10 +150,9 @@ object VanillaGenerator {
                 else -> event(id)
             }
         }
-    }
 
-    @SafeVarargs
-    private fun putMulti(entry: (Item) -> SoundEntry, vararg classez: Class<out Item?>) {
-        for (clazz in classez) map[clazz] = entry
+        else -> {
+            aliased(ITEM_PICK)
+        }
     }
 }
